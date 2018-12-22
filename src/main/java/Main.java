5be5c59b39322e0414b378b3b8c192644a5eb885
lyndemberg <lyndembergbatista@outlook.com>
2018-12-22 16:31:38 -0300

    concorrência das threads funcionando

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,73 @@
 import java.sql.SQLException;
 import java.sql.SQLOutput;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        UserDao userDao = new UserDao();
+        final UserDao userDao = new UserDao();
 
         int contador = 1;
         long t0 = System.currentTimeMillis();
+        final BlockingQueue<Integer> queueInsert = new ArrayBlockingQueue<Integer>(20);
+        final BlockingQueue<Integer> queueUpdate = new ArrayBlockingQueue<Integer>(20);
+        final BlockingQueue<Integer> queueDelete = new ArrayBlockingQueue<Integer>(20);
         while(contador <= 10000){
-            User joao = new User(contador, "JoãoDeDeus");
+            queueInsert.put(contador);
 
-            try {
-                userDao.insert(joao);
-                userDao.update(joao.getId());
-                userDao.delete(joao.getId());
-                contador ++;
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+            Runnable runnableInsert = new Runnable() {
+                public void run() {
+                    try {
+                        Integer take = queueInsert.take();
+                        User user = new User(take,"JoãoDeDeus");
+                        userDao.insert(user);
+                        queueUpdate.put(take);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            Runnable runnableUpdate = new Runnable() {
+                public void run() {
+                    try {
+                        Integer take = queueUpdate.take();
+                        userDao.update(take);
+                        queueDelete.put(take);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            Runnable runnableDelete = new Runnable() {
+                public void run() {
+
+                    try {
+                        Integer take = queueDelete.take();
+                        userDao.delete(take);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            Thread threadInsert = new Thread(runnableInsert);
+            Thread threadUpdate = new Thread(runnableUpdate);
+            Thread threadDelete = new Thread(runnableDelete);
+            threadInsert.start();
+            threadUpdate.start();
+            threadDelete.start();
+
+            contador ++;
+
         }
         long t1 = System.currentTimeMillis();
         long tempoTotal = t1 - t0;
